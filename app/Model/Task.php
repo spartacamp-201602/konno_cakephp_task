3e549185da4e7f55f74e8253429cf963c84b612d
Konno <dra.mixx.nico@gmail.com>
2016-03-20 13:05:03 +0900

    [clean] Taskモデルを見やすく書き直した

--- app/Model/Task.php
+++ app/Model/Task.php
@@ -3,16 +3,16 @@
 class Task extends AppModel {
 
     // アソシエーション設定
-    public $hasMany = array('Note');
+    public $hasMany = ['Note'];
 
-    public $validate = array(
-        'name' => array(
-            'rule' => array('between', 5, 30),
-                'message' => 'タスクは5文字以上30文字以内で入力してください。'
-            ),
-        'body' => array(
-            'rule' => array('maxLength', 255),
-            'message' => '詳細は255文字以内で入力してください。'
-            )
-        );
+    public $validate = [
+        'name' => [
+                    'rule' => ['between', 5, 30],
+                    'message' => 'タスクは5文字以上30文字以内で入力してください。'
+                  ],
+        'body' => [
+                    'rule' => ['maxLength', 255],
+                    'message' => '詳細は255文字以内で入力してください。'
+                  ]
+        ];
 }
